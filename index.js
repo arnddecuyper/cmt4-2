d36412df16c680af1c4c8caf17f553f2560bb7ec
Arnd De Cuyper <decuyperarnd@gmail.com>
2017-05-15 13:42:05 +0200

    ik heb er mooie wereld van gemaakt

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'))
 app.get('/', function(req, res) {
   res.render("index", {
-    naam: "Wereld"
+    naam: "Mooie Wereld"
   });
 });
 app.listen(app.get('port'), function() {
